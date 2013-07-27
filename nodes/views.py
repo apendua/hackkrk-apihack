2ba8fd11ff455e4364fa87bbc37e2c4558eb72d3
Tomasz Lenarcik <tomek.lenarcik@gmail.com>
2013-07-27 02:20:46 +0200

    Custom processing of functions

--- nodes/views.py
+++ nodes/views.py
@@ -11,34 +11,25 @@ cache = {}
 # NODES
 
 def nodes(request, node_id=None):
-
 	if request.method == 'POST' and node_id is None:
 		node = json.loads(request.body) # parse POST data
-
 		err = validate.validateNode(node)
 		if err:
 			return error(err, 422)
-
 		node["id"] = addNode(node)
-
 		return success(node, 201)
-
 	elif request.method == 'GET' and node_id:
-
 		try:
 			node_id = int(node_id)
 		except ValueError:
 			return error()
-
 		node = getNode(node_id)
 		if node:
 			return success(node, 200)
 		else:
 			return error()
-
 	else:
 		return error()
-
 	return error()
 
 def builtin(request, name):
@@ -50,11 +41,17 @@ def builtin(request, name):
 					'body': name,
 				})
 		return success({"id": cache[name]})
-
 	return error()
 
-def functions(request, name):
-	pass
+def functions(request, node_id=None):
+	if request.method == 'POST' and node_id is None:
+		node = json.loads(request.body) # parse POST data
+		node['kind'] = 'function'
+		err = validate.validateNode(node)
+		if err:
+			return error(err, 422)
+		return success({'id':addNode(node)}, 201)
+	return error()
 
 # EVALUATE
 
