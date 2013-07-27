8447bc3b9d0d7d969007c4a461f3b1c1691fda08
Tomasz Lenarcik <tomek.lenarcik@gmail.com>
2013-07-27 13:20:15 +0200

    Better tests

--- test.py
+++ test.py
@@ -2,16 +2,20 @@ import urllib2
 import urlparse
 import json
 
-from urllib2 import urlopen
+from urllib2 import urlopen, HTTPError
 
-#prefix = "http://127.0.0.1:8000"
-prefix = "http://hackkrk-apihack.lenarcik.org:80"
+prefix = "http://127.0.0.1:8000"
+#prefix = "http://hackkrk-apihack.lenarcik.org:80"
 
-def make_request(url, data=None):
-	if data:
-		response = urlopen( urlparse.urljoin(prefix, url), json.dumps(data).encode('utf-8') )
-	else:
-		response = urlopen( urlparse.urljoin(prefix, url) )
+def request(url, data=None):
+	try:
+		if data:
+			response = urlopen( urlparse.urljoin(prefix, url), json.dumps(data).encode('utf-8') )
+		else:
+			response = urlopen( urlparse.urljoin(prefix, url) )
+	except HTTPError, err:
+		print err
+		return
 
 	result = json.loads(response.read().decode('utf-8'))
 	
@@ -23,13 +27,18 @@ def make_request(url, data=None):
 	except KeyError:
 		return None
 
-id0 = make_request("/nodes", {"kind":"constant","type":"int","value":371})
-id1 = make_request("/nodes", {"kind":"constant","type":"int","value":121})
-id2 = make_request("/functions/builtin/add")
-id3 = make_request("/nodes", {"kind":"invoke","function":id2,"arguments":[id0,id1]})
-make_request("/nodes/%i/evaluate" % id3)
+id0 = request("/nodes", {"kind":"constant","type":"int","value":371})
+id1 = request("/nodes", {"kind":"constant","type":"int","value":121})
+id2 = request("/functions/builtin/add")
+id3 = request("/nodes", {"kind":"invoke","function":id2,"arguments":[id0,id1]})
+request("/nodes/%i/evaluate" % id3)
 
-id0 = make_request("/nodes", {"kind":"constant","type":"int","value":406})
-id1 = make_request("/functions", {"body":id0})
-id2 = make_request("/nodes", {"kind":"invoke","function":id1,"arguments":[]})
-id3 = make_request("/nodes/%i/evaluate" % id2)
+id0 = request("/nodes", {"kind":"constant","type":"int","value":406})
+id1 = request("/functions", {"body":id0})
+id2 = request("/nodes", {"kind":"invoke","function":id1,"arguments":[]})
+id3 = request("/nodes/%i/evaluate" % id2)
+
+id0 = request("/nodes", {"kind":"constant","type":"bool","value":True})
+id1 = request("/nodes", {"kind":"constant","type":"string","value":"ZCBCWOVT"})
+id2 = request("/nodes", {"kind":"constant","type":"int","value":682})
+id3 = request("/nodes", {"kind":"if","predicate":id0,"true_branch":id1,"false_branch":id2})
