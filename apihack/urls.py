652db581f47d1b436a26d086d2efeef20a123b4f
Tomasz Lenarcik <tomek.lenarcik@gmail.com>
2013-07-27 03:21:09 +0200

    Fixed patterns to work with "if"

--- apihack/urls.py
+++ apihack/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^nodes$',                                     'nodes.views.nodes'),
-    url(r'^nodes/(?P<node_id>\d+)$',                    'nodes.views.nodes'),
-    url(r'^nodes/(?P<node_id>\d+)/evaluate$',           'nodes.views.evaluate'),
-    url(r'^functions$',                                 'nodes.views.functions'),
-    url(r'^functions/(?P<node_id>\d+)$',                'nodes.views.functions'),
-    url(r'^functions/builtin/(?P<name>(add|mult|lt))$', 'nodes.views.builtin'),
+    url(r'^nodes$',                                        'nodes.views.nodes'),
+    url(r'^nodes/(?P<node_id>\d+)$',                       'nodes.views.nodes'),
+    url(r'^nodes/(?P<node_id>\d+)/evaluate$',              'nodes.views.evaluate'),
+    url(r'^functions$',                                    'nodes.views.functions'),
+    url(r'^functions/(?P<node_id>\d+)$',                   'nodes.views.functions'),
+    url(r'^functions/builtin/(?P<name>(add|mult|lt|if))$', 'nodes.views.builtin'),
 )
